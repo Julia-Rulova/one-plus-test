c1eccf47790e225a0f94e8a8cf5e756e2edcac28
Julia-Rulova <julia.rulova0066@gmail.com>
2023-03-31 12:03:03 +0300

    first done

---
+++ first-test/index.js
@@ -0,0 +1,32 @@
+const tbody = document.querySelector('.tbody');
+
+let dataArr = [];
+
+function getData() {
+    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1')
+    .then(res => res.json())
+    .then(result => dataArr = result)
+    .then(() => {
+        for (let i = 0; i < dataArr.length; i++) {
+            let tr = document.createElement('tr');
+            tr.innerHTML = `
+                        <tr  class="tr">
+                            <td>${dataArr[i].id}</td>
+                            <td>${dataArr[i].symbol}</td>
+                            <td>${dataArr[i].name}</td>
+                         </tr>
+                     `;
+
+            if (dataArr[i].symbol === "usdt") {
+                tr.style.backgroundColor = "rgba(0,255,0,0.5)"
+                
+            } else if (i <= 5) {
+                tr.style.backgroundColor = "rgba(0,0,255,0.5)"
+            }
+
+            tbody.append(tr);
+        }
+    })
+}
+
+getData();
